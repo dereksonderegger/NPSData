bcfbe9fe0d3cc18a2699b469920eb08947f4da7f
Derek Sondereggere <derek.sonderegger@nau.edu>
2016-01-07 16:14:10 -0700

    Adding Jodi's toy dataset

---
+++ R/DataSetDocumentation.R
@@ -0,0 +1,14 @@
+#' abcdPlotGpData
+#'
+#' A data set with the following attributes
+#' @format A data set with columns
+#' \describe{
+#'   \item{PlotID}{A unique identifer for the plot. The first four letters indicate the park.}
+#'   \item{dayN}{Day of Year}
+#'   \item{yearV}{Year}
+#'   \item{NdviMed}{NDVI level. Median NDVI of all sampled cells}
+#'   \item{CellCntScrn}{Number of cells possible for the site.}
+#'   \item{CellCnt}{Number of cells actually used}
+#'   }
+#'   
+"abcdPlotGpData"
